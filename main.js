bc08f7095ea7a031aa7b53d60aad5387ae1961c3
William Castro <williamcastro@users.noreply.github.com>
2020-11-20 16:08:09 -0300

    Enable localstorage persistency

--- main.js
+++ main.js
@@ -1,61 +1,54 @@
 // Variaveis
-let expenseList = document.querySelector(".expenseList")
+let expenseList = document.querySelector(".expenseList");
 let addBtn = document.getElementById("addBtn");
 let input = document.getElementById("inputField");
+let parser = new DOMParser();
 
 // Variaveis auxiliares
-
-
-
-
 let expense;
-// LocalStorage
 let id = 0;
-
-
-addBtn.addEventListener('click', addExpense);
-
 
 // Adicionar despesa a lista
 function addExpense(expense, index) {
-    if (!input.value) {
-        return
-    } else {
-        let expense = input.value;
-        var position = "afterbegin"
+  if (!input.value) {
+    return
+  } else {
+    let expense = input.value;
+    var position = "afterbegin"
+    const entry = `<ul id="list">
+                         <p>R$ ${expense}<img src="trash.jpg"  id="trash"></p>
+                    </ul>`;
 
+    expenseList.insertAdjacentHTML(position, entry);
+    input.value = "";
 
-        const entry = `
-                 <ul id="list">
-                     <p>${expense}R$<img src="trash.jpg"  id="trash"></p>
-                     
-                </ul>
-
-
-                  `;
-        expenseList.insertAdjacentHTML(position, entry);
-
-
-        localStorage.setItem("entry_list", JSON.stringify(entry));
-
-        input.value = "";
-    }
-
-
-
-
-
-
-
+    // Atualiza local storage
+    updateExpensesLocalStorage();
+  }
 }
-expenseList.addEventListener('click', identify)
 
 function identify(event) {
-    var targetBtn = event.target;
-    if (targetBtn.id == "trash") {
-        targetBtn.parentNode.parentNode.parentNode.removeChild(targetBtn.parentNode.parentNode);
-    }
-
-
-
+  var targetBtn = event.target;
+  if (targetBtn.id == "trash") {
+    targetBtn.parentNode.parentNode.parentNode.removeChild(targetBtn.parentNode.parentNode);
+    // Atualiza local storage
+    updateExpensesLocalStorage();
+  }
 }
+
+function updateExpensesLocalStorage() {
+  localStorage.setItem("expenses", expenseList.innerHTML)
+}
+
+// Carrega dados da list do local storage
+function loadExpensesFromLocalStorage() {
+  if (localStorage.getItem("expenses"))
+    expenseList.insertAdjacentHTML('afterbegin', localStorage.getItem("expenses"));
+}
+
+window.addEventListener("load", function (event) {
+  loadExpensesFromLocalStorage();
+});
+
+addBtn.addEventListener('click', addExpense);
+expenseList.addEventListener('click', identify);
